9ebb5d588166c2be739ef8ee639df0df10823542
jenk1 <19733552+jenk1@users.noreply.github.com>
2024-05-31 15:01:13 -0400

    get header finished up

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,10 +1,6 @@
-import { UserButton} from "@clerk/nextjs";
-import {User} from "lucide-react";
-
 export default function Home() {
   return (
       <div>
-        <UserButton afterSignOutUrl="/" />
         <p>This is an authenticated route</p>
       </div>
   );
